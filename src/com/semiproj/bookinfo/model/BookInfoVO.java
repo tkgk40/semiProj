1c09106f0dc5deab71b82e119257227dd9480433
kwony <kwony@LAPTOP-0QF7I9IP>
2020-12-15 17:43:34 +0900

    커밋

---
+++ src/com/semiproj/bookinfo/model/BookInfoVO.java
@@ -0,0 +1,24 @@
+package com.semiproj.bookinfo.model;
+
+import java.sql.Timestamp;
+
+public class BookInfoVO {
+	private int no;
+	private String title;
+	private int price;
+	private Timestamp regdate;
+	private String publisher;
+	private int wrNo;
+	private int cmNo;
+	private String txtFileName;
+	private String coverFileName;
+	private String txtOriginFileName;
+	private String coverOriginFileName;
+	
+	public BookInfoVO() {
+		super();
+	}
+	
+	//
+	
+}
